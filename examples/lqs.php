6237b499a71f3be2dbc8e6d49fcc0ec3cde2fa65
liqingshan <592957170@qq.com>
2019-02-03 16:37:47 +0800

    test

---
+++ examples/lqs.php
@@ -0,0 +1,19 @@
+<?php
+
+$http = new swoole_http_server("127.0.0.1", 9501);
+
+$http->on("request", function ($request, $response) {
+    $client = new Swoole\Coroutine\Client(SWOOLE_SOCK_TCP);
+    $client->connect("127.0.0.1", 8888, 0.5);
+    //调用connect将触发协程切换
+    $client->send("hello world from swoole");
+    //调用recv将触发协程切换
+    $ret = $client->recv();
+    $response->header("Content-Type", "text/plain");
+    $response->end($ret);
+    $client->close();
+});
+
+
+
+$http->start();
