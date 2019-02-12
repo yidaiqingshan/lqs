7b156c7f9585cbbc25d8354b7b5c3c3b10d97688
liqingshan <592957170@qq.com>
2019-02-12 18:03:39 +0800

    test

--- examples/lqs/tcpAsyncClient.php
+++ examples/lqs/tcpAsyncClient.php
@@ -1,10 +1,16 @@
 <?php
 
-$client = new swoole_client(SWOOLE_SOCK_TCP, SWOOLE_SOCK_ASYNC);
+$client = new swoole_client(SWOOLE_SOCK_TCP, SWOOLE_SOCK_ASYNC, SWOOLE_);
 
 //注册连接成功回调
 $client->on("connect", function($cli) {
     $cli->send("开始计算\n");
+    while (1){
+        $a = rand(1,999);
+        $b = rand(1,999);
+        $cli->send($a.' * '.$b.' = '.$a*$b."\n");
+        sleep(2);
+    }
 });
 
 //注册数据接收回调
@@ -26,9 +32,3 @@ $client->on("close", function($cli){
 //发起连接
 $client->connect('127.0.0.1', 10000, 0.5);
 
-while (1){
-  $a = rand(1,999);
-  $b = rand(1,999);
-  $cli->send($a.' * '.$b.' = '.$a*$b."\n");
-  sleep(1);
-}
