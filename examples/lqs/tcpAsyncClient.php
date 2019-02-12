8363516734b1a02bf5fef73d6b9d1012c36c595e
liqingshan <592957170@qq.com>
2019-02-12 19:25:47 +0800

    test

--- examples/lqs/tcpAsyncClient.php
+++ examples/lqs/tcpAsyncClient.php
@@ -5,10 +5,10 @@ go(function (){
 
 //注册连接成功回调
     $client->on("connect", function($cli) {
-        $cli->send("连接成功\n");
-        sleep(1);
-        $cli->send("开始计算\n");
-        sleep(1);
+//        $cli->send("连接成功\n");
+//        sleep(1);
+//        $cli->send("开始计算\n");
+//        sleep(1);
         while (1){
             $a = rand(1,999);
             $b = rand(1,999);
